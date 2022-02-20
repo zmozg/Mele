30307a7888e92c1f8fa15d45d2c88a7951529f42
zmozg <zmozg20@gmail.com>
2022-02-20 21:58:19 +0300

    realy end first chapter

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,6 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     #post views
-    path('', views.post_list, name='post_list'),
+    path('', views.PostList.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 from .models import Post
+
+class PostList(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     posts_all = Post.published.all()
