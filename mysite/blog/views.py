244a2c6a65ed9bc3e5d7c5451ee810177abcf022
Zmozg <Zmozg20@gmail.com>
2022-03-02 12:14:28 +0300

    all 2 chapter comment not work

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
 
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
@@ -43,6 +44,7 @@ def post_list(request, tag_slug=None):
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         posts_all = posts_all.filter(tags__in=[tag])
+    print(tag)
     paginator = Paginator(posts_all, 3)
     page = request.GET.get('page')
     try:
@@ -70,10 +72,16 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:2]
     template = 'blog/post/detail.html'
     values = {'post': post,
                 'comments': comments,
                 'new_comment': new_comment,
                 'comment_form': comment_form,
+                'similar_posts': similar_posts
                 }
     return render(request, template, values)
