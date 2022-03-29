116bd6a1ce268732828d0df30df98a7888598991
zmozg <zmozg20@gmail.com>
2022-03-29 23:01:07 +0300

    reset_password worked

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,16 +1,22 @@
-from django.urls import path, reverse_lazy
+# from xml.etree.ElementInclude import include
+from django.urls import path, reverse_lazy, include
 from django.contrib.auth import views as auth_views
 
 
 from . import views
 
-app_name = 'account'
+
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', views.dashboard, name='dashboard'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('account:password_reset_done')),name='password_reset'),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('account:password_reset_complete')), name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -136,6 +136,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'account:1dashboard'
-LOGIN_URL = 'account:login'
-LOGOUT_URL = 'account:logout'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
