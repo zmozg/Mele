e0818de1f92ab83d5e87ee29f8fd84d7abd51100
Zmozg <Zmozg20@gmail.com>
2022-03-30 16:56:52 +0300

    Edit profile

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -13,10 +13,12 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    # path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('account:password_reset_done')),name='password_reset'),
-    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('account:password_reset_complete')), name='password_reset_confirm'),
-    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('', include('django.contrib.auth.urls')),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
+    # path('register/done/', )
+    # path('', include('django.contrib.auth.urls')),
 ]
